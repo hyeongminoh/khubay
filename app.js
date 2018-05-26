ce6b51ca885ce93a7313c2c4519fc0534e194de3
hyeongminoh <mihn0621@gmail.com>
2018-05-26 17:17:50 +0900

    화면 구성 및 app.js 수정

--- app.js
+++ app.js
@@ -1,26 +1,36 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const ejs = require('ejs');
+const app = express();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
+const db = require('./db.js');
+//const sha256 = require('sha256');
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
 
+//bodyparser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+/*
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/users', usersRouter);*/
+
+const router = require('./routes/router.js')(app);
+
+//for pretty print
+app.locals.pretty = true;
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -38,4 +48,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+const server = app.listen(8000, function () {
+  console.log('Listening on port 8000');
+});
+
+//module.exports = app;
---
+++ routes/router.js
@@ -0,0 +1,90 @@
+module.exports = function(app){
+
+	const db = require('../db');
+	const sha256 = require('sha256');
+
+	app.get('/', function(req,res){
+		res.render('main_index')
+	});
+
+	app.get('/main_index', function (req, res) {
+		res.render('main_index');
+	});
+
+	app.get('/blog_single', function (req, res) {
+		res.render('blog_single');
+	});
+
+	app.get('/blog', function (req, res) {
+		res.render('blog');
+	});
+
+	app.get('/cart', function (req, res) {
+		res.render('cart');
+	});
+
+	app.get('/error;', function (req, res) {
+		res.render('error');
+	});
+
+	app.get('/product', function (req, res) {
+		res.render('product');
+	});
+
+	app.get('/regular', function (req, res) {
+		res.render('regular');
+	});
+
+	app.get('/shop', function (req, res) {
+		res.render('shop');
+	});
+
+	app.get('/signin', function (req, res) {
+		res.render('signin');
+	});
+
+	app.get('/shop', function (req, res) {
+		res.render('shop');
+	});
+
+	app.get('/registration', function (req, res) {
+		res.render('registration');
+	});
+
+	app.get('/product_register', function (req, res) {
+		res.render('product_register');
+	});
+	app.get('/category', function (req, res) {
+		res.render('category');
+	});
+	app.get('/category/:id', function (req, res) {
+		res.render('category');
+	});
+
+	app.post("/test_registration", function (req,res){
+			console.log("test_registration connect");
+			 var body = req.body;
+			 var email = body.InputEmail;
+			 var name = body.InputUserName;
+			 var passwd = sha256(body.InputPassword1);
+			 var nickname = body.InputUserNickname;
+			 var phone = body.InputPhoneNum;
+			 var birth = body.InputBirth;
+			 var address = body.InputAddress
+			console.log(email, name);
+
+			/* var query = db.query('INSERT INTO user (user_email, user_pw, user_name, user_nickname, user_phone, user_birth, user_address, user_chk ) VALUES ("' + email + '","' + passwd + '","' + name + '","' + nickname + '","' + phone + '","'  + birth + '","'  + address + + '","' '")', function(err, rows) {
+					 if(err) { console.log(err);}
+					 console.log("err!");
+			 })*/
+
+			db.query('INSERT INTO user(user_email, user_pw, user_name, user_nickname, user_phone, user_birth, user_address, user_chk ) VALUES(?,?,?,?,?,?,?,?) ',
+			[email, passwd, name, nickname, phone, birth, address, '1'], function(error,result){
+				if(error) throw error;
+				console.log('추가 완료. result: ',result);
+			});
+
+			 res.redirect('/registration')
+	 	});
+
+}
