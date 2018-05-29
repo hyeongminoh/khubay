e397dcd92ccd4f2cd2f426bf4742918439bceb36
Yoonmee <ymhwang119@naver.com>
2018-05-30 04:24:39 +0900

    block data 추가

--- java/src/main/java/agent/Block.java
+++ java/src/main/java/agent/Block.java
@@ -11,12 +11,69 @@ public class Block implements Serializable {
     public static String sayHello(){
       return "Hello!!";
     }
+
+    public class BidData{
+      int user_id; //경매 참여 유저 아이디
+      int item_id; //물건 id
+      int bidding_price; //유저 비딩 가격
+      int auto_bid_price; //만약 auto bid 할거면 가격, 아니면 -1
+      Date bid_time; //비딩 시각
+      int highest_price; //등록 당시 제일 최고값?
+
+      //toString biddata 정보 출력
+      public String toString() {
+        return "BidData{" +
+                "user_id=" + user_id +
+                ", item_id=" + item_id +
+                ", bidding_price='" + bidding_price + '\'' +
+                ", auto_bid_price='" + auto_bid_price + '\'' +
+                ", bid_time='" + bid_time +
+                ", highest_price='" + highest_price +
+                 '}';
+     }
+
+     //생성자
+     public BidData(int _user_id, int _item_id, int _bidding_price, int _auto_bid_price, Date _bid_time, int _highest_price)
+     {
+       this.user_id = _user_id;
+       this.item_id = _item_id;
+       this.bidding_price = _bidding_price;
+       this.auto_bid_price = _auto_bid_price;
+       this.bid_time = _bid_time;
+       this.highest_price = _highest_price;
+     }
+
+     public int getUserID() {
+         return user_id;
+     }
+
+     public int getItemID() {
+         return item_id;
+     }
+
+     public int getBiddingPrice() {
+         return bidding_price;
+     }
+
+     public int getAutoBidPrice() {
+         return auto_bid_price;
+     }
+
+     public Date getBidTime() {
+         return bid_time;
+     }
+
+     public int getHighestPrice() {
+         return highest_price;
+     }
+    }
+
     private int index;
     private Long timestamp;
     private String hash;
     private String previousHash;
     private String creator;
-    private
+    private BidData data;
 
     // for jackson
     public Block() {
@@ -28,8 +85,9 @@ public class Block implements Serializable {
                 "index=" + index +
                 ", timestamp=" + timestamp +
                 ", creator=" + creator +
-//                ", hash='" + hash + '\'' +
-//                ", previousHash='" + previousHash + '\'' +
+               ", hash='" + hash + "'\'" +
+               ", previousHash='" + previousHash +  "'\'"  +
+               ", data='" +  "'\'"  + data.toString() +
                 '}';
     }
 
